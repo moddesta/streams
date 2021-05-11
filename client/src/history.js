2c389387ea67cd71e38dcd6dca2bf8063a15848b
Modesta Strolyte <modesta@Martynass-MacBook-Pro.local>
2021-05-11 11:47:05 +0300

    client folder added

---
+++ client/src/history.js
@@ -0,0 +1,8 @@
+import { createBrowserHistory } from 'history'; 
+
+export default createBrowserHistory();
+
+
+
+//import createHistory from 'history/createBrowserHistory'; 
+//export default createHistory();
